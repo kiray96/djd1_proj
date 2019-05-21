d5bafaf8d13d68a821d39e2873236a9665e70a4f
LegendaryBananaCat <joana.f.silva.99@gmail.com>
2019-05-21 22:38:35 +0100

    The 300 V1

---
+++ Assets/Scripts/ParalaxObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxObj : MonoBehaviour
+{
+    public Camera masterCamera;
+    public float paralaxScale = 0.1f;
+
+    Vector3 originalPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position - masterCamera.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = masterCamera.transform.position * paralaxScale + originalPosition;
+    }
+}
