ad8603187bccb98fdc5629127a03d9ee88e6a3b3
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2019-05-21 03:50:24 +0100

    first commit - cowabunga it is

---
+++ Assets/Scripts/Army.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Army : MonoBehaviour
+{
+
+    protected EnumArmyStates currentState;
+    [SerializeField] protected int nTroops;
+    [SerializeField] protected float speed;
+
+    // How much mass is each soldier worth
+    [SerializeField] protected float troopWeight;
+
+
+    protected float massModifier;
+
+}
---
+++ Assets/Scripts/EnumArmyStates.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumArmyStates
+{
+       Brace,
+       March,
+       Cover,
+       Charge,
+       AttackPush,
+       Projectile,
+       SendKing 
+}
