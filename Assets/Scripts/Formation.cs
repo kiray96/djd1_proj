d85032b090b62430d622391c96d9822d9e5c5c8a
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2019-05-23 02:07:10 +0100

    added formation.cs mother class to all formations, long may she reign

---
+++ Assets/Scripts/Formation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Formation : MonoBehaviour
+{
+    float speedMod;
+    float atkMod;
+    float defMod;
+    float massMod;
+
+    EnumArmyStates currFormation;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedMod = Mathf.Clamp(speedMod, 0, 1);
+        atkMod = Mathf.Clamp(atkMod, 0, 1);
+        defMod = Mathf.Clamp(defMod, 0, 1);
+        massMod = Mathf.Clamp(massMod, 0, 1);
+    }
+}
