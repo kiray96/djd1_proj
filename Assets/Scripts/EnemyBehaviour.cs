ff8e9b53bb73dd9efa2e5b711548d6c73f336877
RafaelCS-Aula <castroesilca.rafael@gmail.com>
2019-05-25 16:11:52 +0100

    Finished( big sphaguetti ). Ai to have enemy choose what formation he'll use

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    
+
     [SerializeField]
     float minDistanceToCharge;
     [SerializeField]
     float minDistanceToBrace;
-   
-
+    [SerializeField]
+    float chanceToDoNothing = 0.20f;
 
     float distanceToPlayer;
 
     float agressiveMod;
 
-    float choiceWeight;
-
     float chanceToBrace;
     float chanceToCharge;
-    float chanceToDoNothing = 0.20f;
+    
 
 
     Formation[] possibleStates;
@@ -28,6 +28,9 @@ public class EnemyBehaviour : MonoBehaviour
 
 
     GameObject playerArmy;
+
+    Attack atkScript;
+
     private void Awake()
     {
         // Gets all components whpo inherit from the Formation class
@@ -46,7 +49,7 @@ public class EnemyBehaviour : MonoBehaviour
 
         }
 
-        chanceList = new float[possibleStates.Length];
+        chanceList = new float[possibleStates.Length + 1];
            
     }
 
@@ -56,14 +59,16 @@ public class EnemyBehaviour : MonoBehaviour
 
         Mathf.Clamp(chanceToCharge, 0, 1);
         Mathf.Clamp(chanceToBrace, 0, 1);
+        Mathf.Clamp(agressiveMod, 0, 1);
 
         
         // Get distance to player
         distanceToPlayer = (playerArmy.transform.position - gameObject.transform.position).magnitude;
 
+        Formation next = CalculateNextState();
+        if (next != null)
+            next.ActivateFormation();
 
-
- 
 
     }
 
@@ -85,6 +90,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     }
 
+
     void CalculateChanceToBrace()
     {
         // Enemy braces sooner if less agressive
@@ -100,5 +106,50 @@ public class EnemyBehaviour : MonoBehaviour
 
     }
 
+    Formation CalculateNextState()
+    {
+        float sum = 0.0f;
+        chanceList[0] = chanceToDoNothing;
 
+        // REMINDER:
+        // chanceList has 1 more value than possibleStates, this is the chance 
+        // that the enemy will not change his state.
+
+        for (int i = 0; i < possibleStates.Length; i++)
+        {
+            if (possibleStates[i] as FCharge != null) chanceList[i + 1] = chanceToCharge;
+            if (possibleStates[i] as FBrace != null) chanceList[i + 1] = chanceToBrace;
+            // more chances for more formation spossible
+        }
+
+
+        foreach (float f in chanceList)
+        {
+            sum += f;
+        }
+
+        float r = Random.Range(0, sum);
+
+
+        if (sum < chanceList[0])
+        {
+            return null;
+        }
+
+        sum -= chanceList[0];
+
+        for (int i = 1; i < chanceList.Length - 1; i++)
+        {
+            if (sum < chanceList[i])
+            {
+                return possibleStates[i - 1];
+
+            }
+
+            sum -= chanceList[i];
+        }
+
+        return null;
+
+    }
 }
